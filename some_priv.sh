9176c9e8fcf2362d8d745826040d8dd25062bfa6
Michael Devey <mdevey@users.noreply.github.com>
2016-09-02 00:03:16 -0700

    privileges to allow mounted filesystems in docker

---
+++ some_priv.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+
+if [ "$HOME" = '/home/user' ]; then
+	echo >&2 'uh oh, HOME=/home/user'
+	exit 1
+fi
+
+mkdir -p "$HOME/Sync"
+mkdir -p "$HOME/.config/syncthing"
+
+set -x
+docker run -d \
+	--cap-add SYS_ADMIN \
+	--cap-add DAC_READ_SEARCH \
+	--name syncthing \
+	--restart always \
+	--user "$(id -u):$(id -g)" \
+	-v "$HOME/Sync:$HOME/Sync" \
+	-v "$HOME/.config/syncthing:/home/user/.config/syncthing" \
+	-p 8384:8384 \
+	-p 22000:22000 \
+	-p 21027:21027/udp \
+	mdevey/syncthing "$@"
+timeout 10s docker logs -f syncthing || true
